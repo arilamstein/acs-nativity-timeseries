7b3f8a02e75a6202656eb0174d4478d50d38424e
Ari Lamstein <arilamstein@gmail.com>
2026-03-02 15:53:06 -0800

    Make acs_nativity.__version__ work

--- src/acs_nativity/__init__.py
+++ src/acs_nativity/__init__.py
@@ -1,8 +1,12 @@
+from importlib.metadata import version
+
 from .data import get_nativity_timeseries
 from .plotting import (
     plot_nativity_timeseries,
     plot_nativity_change,
 )
+
+__version__ = version("acs-nativity")
 
 __all__ = [
     "get_nativity_timeseries",
